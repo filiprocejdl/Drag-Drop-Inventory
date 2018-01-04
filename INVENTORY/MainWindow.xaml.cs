c571d04a4b771291246bd6117b4eef86670ded95
rocejfi15 <rocejfi15@DST20717>
2018-01-04 12:34:01 +0100

    Změna #2

--- INVENTORY/MainWindow.xaml.cs
+++ INVENTORY/MainWindow.xaml.cs
@@ -20,9 +20,43 @@ namespace INVENTORY
     /// </summary>
     public partial class MainWindow : Window
     {
+        Rectangle obj;
+        private bool _isRectDragInProg;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = true;
+            obj = sender as Rectangle;
+
+            obj.CaptureMouse();
+        }
+
+        private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = false;
+            obj.ReleaseMouseCapture();
+        }
+
+        private void rect_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isRectDragInProg) return;
+
+            // get the position of the mouse relative to the Canvas
+            var mousePos = e.GetPosition(grid);
+
+            // center the rect on the mouse          
+            double left = mousePos.X  / 50;
+            double top = mousePos.Y  / 50;
+
+            int promleft = (int)left;
+            int promright = (int)top;
+
+            Grid.SetColumn(obj, promleft);
+            Grid.SetRow(obj, promright);
+        }   
     }
 }
